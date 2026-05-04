fba4414b4cf9761d3e262dfc466bfe0efacac797
Rodrigo Neves da Silva <rodrigoneves1203@gmail.com>
2026-05-04 15:24:07 -0300

    v

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("projeto"));
+app.use('/fotos', express.static('fotos'));
 
 // ROTAS
 const rota_ciclo = require("./routes/ciclosRoutes");
