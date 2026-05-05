192bd53d37968a1aa52cf3df78d60a559e7f253e
Rodrigo Neves da Silva <rodrigoneves1203@gmail.com>
2026-05-05 20:26:24 -0300

    corrigido

--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -5,7 +5,6 @@ const Controller = require('../controllers/usuarioController');
 rotas.post('/login', Controller.login);
 rotas.post('/cadastro', Controller.cadastro);
 rotas.post('/alterar', Controller.alterarSenha);
-rotas.post('/recuperar-senha',Controller.pedirSenha);
 rotas.post('/nova-senha',Controller.novaSenha);
 rotas.put('/usuario/:id', Controller.atualizarDadosUser);
 rotas.get('/confirmar-email', Controller.confirmar);
