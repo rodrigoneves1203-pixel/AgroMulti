2fb0effc9d6fbd3c3f4901718443c4fb4a274f7c
Rodrigo Neves da Silva <rodrigoneves1203@gmail.com>
2026-05-07 18:41:09 -0300

    f

--- projeto/html/script/relatorios.js
+++ projeto/html/script/relatorios.js
@@ -59,8 +59,8 @@ let grafico;
 
 async function gerarGrafico(id_ciclo){
 
-    const gastosRes = await fetch(`https://agromulti-2.onrender.com/gastos/${id_ciclo}`);
-    const receitasRes = await fetch(`https://agromulti-2.onrender.com/receita/${id_ciclo}`);
+    const gastosRes = await fetch(`https://agromulti-2.onrender.com/gastos/ciclo/${id_ciclo}`);
+    const receitasRes = await fetch(`https://agromulti-2.onrender.com/receita/ciclo//${id_ciclo}`);
 
     const gastos = await gastosRes.json();
     const receitas = await receitasRes.json();
